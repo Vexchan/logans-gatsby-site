f4b1feffdcd243f0b66d976cf263400c63724202
Logan Pickett <logan_pickett4@mymail.eku.edu>
2022-09-14 11:39:44 -0400

    Added pass

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-
+/* 
+pass: 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
+*/
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
