5e64b202563cbb9bf2a6cc8e382bc2930a2f50a9
Logan Pickett <logan_pickett4@mymail.eku.edu>
2022-09-14 11:47:55 -0400

    Finished third part of Gatsby tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
-import * as React from 'react'
-import Layout from '../components/layout'
 /* 
 pass: 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
 */
+import * as React from 'react'
+import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="A bloody rainbow!!!"
+        src="../images/rainbow.jpg"
+      />
     </Layout>
   )
 }
