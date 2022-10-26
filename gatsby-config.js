5b4aa96fe31dad4ed344d91d0b6490da5d1c9314
Logan Pickett <logan_pickett4@mymail.eku.edu>
2022-10-26 15:42:38 -0400

    Forgot some files

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,20 @@ module.exports = {
     },
     "gatsby-plugin-mdx",
     "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // This type will contain remote schema Query type
+        typeName: "DrupalGraqhQL",
+        // This is field under which it's accessible
+        fieldName: "Drupal",
+        // Url to query from
+        // url: "http://kecatalog.dev.wwbtc.com/graphql",
+        //  url: `http://10.89.0.11/graphql`,
+        // url: `https://api.keeneland.com/graphql`,
+        // url: "http://10.89.0.24/graphql",
+        url: `https://csc496f22demo.tldr.dev/graphql`,
+      },
+    },
   ],
 };
